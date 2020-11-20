1e1bd25de2f105b8043b82d03d4ca6685b01d314
hamzao777555 <hamzao777555@gmail.com>
2020-11-20 20:10:19 +0300

    Initial Commit

---
+++ socialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  socialApp
+//
+//  Created by Hamza on 20/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
